4e8dc34ac7ec259383aeafd2729b4a8c3ef40f13
Max Mushkin <maxfly@outlook.com>
2019-04-24 20:25:54 +0300

    Initial commit of the SQL DB Publisher

---
+++ src/DynamicsConnector.SqlDb.Functions/SqlDBProviderFunction.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using DynamicsConnector.Core.Models;
+using DynamicsConnector.Core.Providers;
+using DynamicsConnector.SqlDb;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace DynamicsConnector.SqlDb.Functions
+{
+    public static class SqlDBProviderFunction
+    {
+        private static ConnectionStringSettings dataBaseConnectionString = ConfigurationManager.ConnectionStrings["SourceDB_CS"];
+        private static string sourceDataTable = ConfigurationManager.AppSettings["SourceDataTable"];
+
+        private static string serviceBusConnectionString = ConfigurationManager.AppSettings["ServiceBusCS"];
+        private static string dynamicsInstanceCreateQueue = ConfigurationManager.AppSettings["DynamicsInstanceCreateQueue"];
+        private static string dynamicsEntityName = ConfigurationManager.AppSettings["DynamicsEntityName"];
+        private static string dynamicsEntityToken = ConfigurationManager.AppSettings["DynamicsEntityToken"];
+        private static string missingMappingField = ConfigurationManager.AppSettings["MissingMappingField"];
+        private static bool update = false;
+
+        [FunctionName("SqlDbProviderFunction")]
+        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger logger)        
+        {           
+            RunAsync(logger).GetAwaiter().GetResult();
+            return;
+        }
+
+        public static async Task RunAsync(ILogger logger)
+        {
+            var currentTime = DateTime.Now;
+            logger.LogInformation($"Sql DB Publisher Function has started at {currentTime}");
+
+            DbAdapter ff = new DbAdapter(dataBaseConnectionString.ConnectionString, logger);
+            IDataDestinationProvider dataDestination = new ServiceBusProvider();
+
+            var dbEntities = ff.GetItems(sourceDataTable);
+
+            if (ConfigurationManager.AppSettings["Update"] != null)
+                bool.TryParse(ConfigurationManager.AppSettings["Update"], out update);
+
+            if (dbEntities.Any())
+            {
+                bool update = (currentTime.Hour == 0);
+
+                logger.LogInformation($"Starting iterating over Faults, update state is {update}.");                
+
+                foreach (var sourceItem in dbEntities)
+                {
+                    var message = new InstanceCreateMessage(dynamicsEntityName, dynamicsEntityToken, sourceItem, update) { MissingMappingField = missingMappingField };
+
+                    await dataDestination.PostDataAsync<InstanceCreateMessage>(message, new string[] { serviceBusConnectionString, "", dynamicsInstanceCreateQueue, "" });
+                    break;
+                }
+
+                logger.LogInformation("Iteration over Faults is completed!");
+            }
+
+            return;
+        }
+    }
+}
---
+++ src/DynamicsConnector.SqlDb/DbAdapter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DynamicsConnector.Core.Providers;
+using Microsoft.Extensions.Logging;
+
+namespace DynamicsConnector.SqlDb
+{
+    public class DbAdapter
+    {
+        private string cs;
+        private ILogger Logger;
+
+        public DbAdapter(string cs, ILogger logger)
+        {
+            this.cs = cs;
+            this.Logger = logger; 
+        }
+
+        public List<IDictionary<string, object>> GetItems(string sourceTable)
+        {
+            try
+            {
+                var dataSource = new SqlDBAdapter();
+
+                Logger.LogInformation($"Retriving data from {sourceTable}.");
+
+                var items = dataSource.DapperSelect(cs, $"SELECT * FROM [dbo].[{sourceTable}]");
+
+                Logger.LogInformation($"Found {items.Count} items.");
+
+                return items;
+            }
+            catch(Exception ex)
+            {
+                Logger.LogError(ex.Message);
+                return null;
+            }
+        }        
+    }
+}
